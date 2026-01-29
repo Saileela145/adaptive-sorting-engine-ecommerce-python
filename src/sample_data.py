f9846e587716b5091d7f5c7c2b1ea22dd26a6f3b
SAILEELA D <156500159+Saileela145@users.noreply.github.com>
2026-01-29 20:42:41 +0530

    Add sample data for products in sample_data.py

---
+++ src/sample_data.py
@@ -0,0 +1,13 @@
+
+
+from product import Product
+
+def get_sample_products():
+    return [
+        Product(1, "Smartphone A", 15000, 4.3, "2024-11-01"),
+        Product(2, "Smartphone B", 12000, 4.1, "2024-10-15"),
+        Product(3, "Smartphone C", 15000, 4.6, "2024-11-10"),
+        Product(4, "Smartphone D", 18000, 4.8, "2024-09-05"),
+        Product(5, "Smartphone E", 12000, 3.9, "2024-11-12"),
+        Product(6, "Smartphone F", 22000, 4.5, "2024-08-20"),
+    ]
